acfe3f3b7f9e76e0f3230dbf1a8d907ab0d8a64f
ragnar <mystparkkorea@gmail.com>
2022-11-27 12:21:26 +0900

    사용자 입력 state관리 및 검증

--- react-practice-project/src/Users/AddUser.js
+++ react-practice-project/src/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Card from '../UI/Card';
 import Button from '../UI/Button'
@@ -6,17 +6,38 @@ import classes from './AddUser.module.css'
 
 
 const AddUser = props => {
+    const [enteredUserName, setEnteredUsername] = useState(''); 
+    const [enteredAge, setEnteredAge] = useState(''); 
+
+    const usernameChangeHandler = (event) => {
+        setEnteredUsername(event.target.value);
+    }
+
+    const ageChangeHandler = (event) => {
+        setEnteredAge(event.target.value);
+    }
+
     const addUserHandler = (event) => {
         event.preventDefault();
+        if (enteredUserName.trim().length === 0 || enteredAge.trim().length === 0) {
+            return;
+        }
+        if (+enteredAge < 1) {
+            // 입력된 나이값을 숫자로 캐스팅
+            return;
+        }
+        console.log(enteredUserName, enteredAge)
+        setEnteredUsername('');
+        setEnteredAge('')
     };
 
     return (
     <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
             <label htmlFor="username">Username</label>
-            <input id="username" type="text"></input>
+            <input id="username" type="text" value={enteredUserName} onChange={usernameChangeHandler}></input>
             <label htmlFor="age">Age (Years)</label>
-            <input id="age" type="number"></input>
+            <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler}></input>
             <Button type="submit">Add User</Button>
         </form>
     </Card>    
