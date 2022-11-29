fdf22d0aec8523ab86f788ef5cb7861f35f32286
ragnar <mystparkkorea@gmail.com>
2022-11-29 09:18:06 +0900

    Wrapper컴포넌트 사용

--- react-practice-project/src/components/Users/AddUser.js
+++ react-practice-project/src/components/Users/AddUser.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import Card from '../UI/Card';
 import Button from '../UI/Button';
 import ErrorModal from '../UI/ErrorModal';
+import Wrapper from '../Helperr/Wrapper';
 import classes from './AddUser.module.css';
 
 const AddUser = props => {
@@ -45,8 +46,14 @@ const AddUser = props => {
     }
 
     return (
-    <div>
-        {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}
+    <Wrapper>
+        {error &&(
+            <ErrorModal 
+                title={error.title} 
+                message={error.message} 
+                onConfirm={errorHandler}  
+            />
+        )}
         <Card className={classes.input}>
             <form onSubmit={addUserHandler}>
                 <label htmlFor="username">Username</label>
@@ -56,7 +63,7 @@ const AddUser = props => {
                 <Button type="submit">Add User</Button>
             </form>
         </Card>    
-    </div>
+    </Wrapper>
     )
 };
 
