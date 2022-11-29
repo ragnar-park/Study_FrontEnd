fe315958e669f4720fcc53a7c5092cde9471ae50
ragnar <mystparkkorea@gmail.com>
2022-11-29 14:44:54 +0900

    자바스크립트에서 타입의 필요성

---
+++ TypeScript/why-ts/sample.js
@@ -0,0 +1,20 @@
+// function sum(a, b) {
+//   return a + b;
+// }
+// sum(10, "20"); // 1020
+// var result_js = sum(10, 20);
+// result_js.toLocaleString();
+
+// @ts-check
+
+/**
+ *
+ * @param {number} a 첫번째 숫자
+ * @param {number} b 두번째 숫자
+ * @returns
+ */
+function sum(a, b) {
+  return a + b;
+}
+
+sum(10, 20);
---
+++ TypeScript/why-ts/sample.ts
@@ -0,0 +1,5 @@
+function add(a: number, b: number): number {
+  return a + b;
+}
+var result_ts = add(10, 20);
+result_ts.toLocaleString();
