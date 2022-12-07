a99b34533e94cb7c8e09a80453c3ca8f9bd30f18
ragnar <mystparkkorea@gmail.com>
2022-12-07 21:31:41 +0900

    제네릭 타입 제한

--- TypeScript/class-note/8_generics.ts
+++ TypeScript/class-note/8_generics.ts
@@ -46,3 +46,57 @@ const abc = logText<string>('abc');
 abc.split('');
 
 const flag = logText<boolean>(true);
+
+// 인터페이스에 제네릭을 선언하는 방법
+// interface Dropdown {
+//     value: string;
+//     selected: boolean;
+// }
+
+// const obj: Dropdown = { value: 'abc', selected: false }; // value에 string만 가능하다
+
+interface Dropdown<T> { 
+    value: T;
+    selected: boolean;
+}
+
+const obj: Dropdown<number> = { value: 5, selected: false}; 
+
+// 제네릭의 타입 제한
+// function logTextLength<T>(text: T[]): T[]{
+//     console.log(text.length);
+//     text.forEach(function (text) {
+//         console.log(text);
+//     });
+//     return text;
+// }
+// logTextLength<string>(['hi','abc']);
+
+// 제네릭 타입 제한 2 - 정의된 타입 이용하기
+interface LengthType {
+    length: number;
+}
+function  logTextLength<T extends LengthType>(text: T): T{
+    text.length;
+    return text;
+}
+logTextLength('ronaldo');
+logTextLength({ length: 10 }); 
+logTextLength({ text: 10 }); // 에러
+logTextLength(10); // 에러
+
+// 제네릭 타입 제한 3 - keyof
+interface ShoppingItem {
+    name: string;
+    price: number;
+    stock: number;
+}
+
+function getShoppingItemOption<T extends keyof ShoppingItem>(itemOption: T): T {
+    return itemOption;
+}
+// getShoppingItemOption(10);
+// getShoppingItemOption<string>('a');
+getShoppingItemOption("name");
+getShoppingItemOption("price");
+getShoppingItemOption("stock");
