70d5972e9afb42f73984c4f8ee6ec8f72d6020bb
ragnar <mystparkkorea@gmail.com>
2022-12-28 21:05:27 +0900

    유틸리티 타입 - Omit 타입

--- TypeScript/class-note/13_utility-type.ts
+++ TypeScript/class-note/13_utility-type.ts
@@ -10,7 +10,7 @@ interface Product {
 function fetchProducts(): Promise<Product[]> {
     // ...
 }
-
+// 타입스크립트 유틸리티 타입 공식문서: https://www.typescriptlang.org/docs/handbook/utility-types.html
 
 // interface ProductDetail {
 //     id: number; 
@@ -22,4 +22,23 @@ function fetchProducts(): Promise<Product[]> {
 type shoppingItem = Pick<Product, 'id' | 'name' | 'price'>
 function displayProductDetail(shoppingItem: Pick<Product, 'id' | 'name' | 'price'>) {
 
+}
+
+//Omit 특정 타입에서 지정된 속성만 제거한 타입을 정의
+interface AddressBook {
+    name: string;
+    phone: number;
+    address: string;
+    company: string;
+}
+
+const phoneBook: Omit<AddressBook, 'address'> = {
+    name: '재택근무',
+    phone: 8201012341234,
+    company: '내 방'
+}
+
+const chingtao: Omit<AddressBook, 'address'|'company'> = {
+    name: '중국집',
+    phone: 11112222
 }
