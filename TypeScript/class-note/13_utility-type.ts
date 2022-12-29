0f6eead9e21c5a5ecd9bce98639f891be06cb22c
ragnar <mystparkkorea@gmail.com>
2022-12-29 21:29:26 +0900

    유틸리티 타입 구현

--- TypeScript/class-note/13_utility-type.ts
+++ TypeScript/class-note/13_utility-type.ts
@@ -6,7 +6,7 @@ interface Product {
     stock: number;
 }
 
-// 상품 목록을 받아오기 위한 API 함수
+// 1. 상품 목록을 받아오기 위한 API 함수
 function fetchProducts(): Promise<Product[]> {
     // ...
 }
@@ -19,10 +19,66 @@ function fetchProducts(): Promise<Product[]> {
 // }
 // pick을 사용해 불필요한 타입 정의를 줄일 수 있다.
 
+// 2. 특정 상품의 상세 정보를 나타내기 위한 함수
 type shoppingItem = Pick<Product, 'id' | 'name' | 'price'>
 function displayProductDetail(shoppingItem: Pick<Product, 'id' | 'name' | 'price'>) {
 
 }
+
+// interface UpdateProduct {
+//     id?: number;
+//     name?: string;
+//     price?: number;
+//     brand?: string;
+//     stock?: number;
+// }
+
+//  Partial<Product>는 위의 정의한 UpdateProduct 인터페이스를 사용하는 것과 동일한 효과
+type UpdateProduct = Partial<Product>
+
+
+// 3. 특정 상품 정보를 업데이트(갱신)하는 함수
+// 필요 정보만 업데이트 
+function updateProductItem(productItem: UpdateProduct) {
+
+
+}
+
+// 4. 유틸리티 타입 구현하기 - Partial
+interface UserProfile {
+    username: string;
+    email: string;
+    profilePhotoUrl: string;
+}
+
+// interface UserProfileUpdate {
+//     username?: string;
+//     email?: string;
+//     profilePhotoUrl?: string;
+// }
+// #1
+// type UserProfileUpdate = {
+//     username?: UserProfile['username'];
+//     email?: UserProfile['email'];
+//     profilePhotoUrl?: UserProfile['profilePhotoUrl'];
+// }
+
+// #2 (맵드 타입)
+// type UserProfileUpdate = {
+//     [ p in 'username' | 'email' | 'profilePhotoUrl']?: UserProfile[p]
+// }
+// type UserProfileKeys = keyof UserProfile;
+
+// #3 
+type UserProfileUpdate = {
+    [ p in keyof UserProfile]?: UserProfile[p]
+}
+
+// #4 Partial의 구현체와 동일
+type  Subset<T> = {
+    [ p in keyof T]?: T[p];
+}
+
 
 //Omit 특정 타입에서 지정된 속성만 제거한 타입을 정의
 interface AddressBook {
