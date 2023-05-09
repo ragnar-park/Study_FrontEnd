110a2e8433dfa6b78a9fe2209451447364474605
ragnar <mystparkkorea@gmail.com>
2023-05-09 22:33:07 +0900

    다른 라우트 사용 및 bodyParser사용

--- nodeJs/NODEJS-COMPLETE-GUIDE/app.js
+++ nodeJs/NODEJS-COMPLETE-GUIDE/app.js
@@ -2,25 +2,50 @@
 /* express */
 const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 
 const app = express();
 
 // 미들웨어 등록
-app.use((req, res, next) => {
-    console.log('In the middleware!');
-    next();
+// app.use((req, res, next) => {
+//     console.log('In the middleware!');
+//     next();
+// });
+
+// app.use('/',(req, res, next) => {
+//     console.log('This always runs!');
+//     next();
+// });
+
+// 요청 본문 분석 미들웨어
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product',(req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">제출</button></form>'); 
+    // next(); 하나의 요청만 보낼 수 있다. 
 });
 
-app.use((req, res, next) => {
+app.use('/product',(req,res, next) => {
+    console.log(req.body)
+    // bodyParser.urlencoded을 미들웨어 등록을 하지 않으면 undifind이다. 
+    // req가 본문에 편의 속성을 주지만 기본적으로 req는 들어오는 요청의 본문을분석하려 하지 않기 때문이다.
+    // 따라서 분석기를 등록 해줘야 한다.
+    
+    res.redirect('/');
+});
+
+app.use('/',(req, res, next) => {
     console.log('In another middleware!');
     res.send('<h1>Hello from Express!</h1>'); // 기본 응답 헤더는 text/html 
     // next()를 호출 하지 않음에도 더 이상 요청 실패가 없음, send로 응답하고 있기 때문
 });
 
-const server = http.createServer(app);
-server.listen(3000); 
-
+// express에서 내부적으로 동일한 작업을 수행
+// const server = http.createServer(app);
+// server.listen(3000); 
+app.listen(3000);
 
 
 
