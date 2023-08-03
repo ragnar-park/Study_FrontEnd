0dc43f83c7120cbfee54e4e0052618afb6210b59
ragnar <mystparkkorea@gmail.com>
2023-08-03 16:32:38 +0900

    플렉스 박스 적용

--- ReactNative/RNBasic/App.js
+++ ReactNative/RNBasic/App.js
@@ -1,26 +1,33 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View>
-        <Text style={styles.dummyText}>Another piece of text!</Text>
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder='your course goal!' />
+        <Button title='Add Goal'/>
       </View>
-      <Text style={styles.dummyText}>Heloo World!</Text>
-      <Button title='Tab me!'/>
+      <View>
+        <Text>List of goals...</Text>
+      </View>
     </View>
   );
 }
 
 // 아래에 정의되었지만 jsx에서 사용할 수 있는 건 전체 코드 파일이 파싱된 후에 실행되기 때문
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer: {
+    padding: 50
   },
-  dummyText: {
-    margin: 16, borderWidth: 1, borderColor: 'blue', padding: 16
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '80%',
+    marginRight: 8,
+    padding: 8,
   }
 });
