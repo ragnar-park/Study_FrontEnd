62ee360e197e9c9fb3557f41ae1b2409d49df74a
ragnar <mystparkkorea@gmail.com>
2023-08-03 16:49:38 +0900

    UI 개선 및 이벤트 처리

--- ReactNative/RNBasic/App.js
+++ ReactNative/RNBasic/App.js
@@ -1,13 +1,26 @@
+import { useState } from 'react';
 import { StyleSheet, Text, View, Button, TextInput} from 'react-native';
 
 export default function App() {
+
+  const [enteredGoalText, setEnteredGoalText] = useState('');
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoalText(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    console.log(enteredGoalText);
+  };
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder='your course goal!' />
-        <Button title='Add Goal'/>
+        <TextInput style={styles.textInput} placeholder='your course goal!' onChangeText={goalInputHandler}/>
+        {/* Button은 스타일 프로퍼티를 지원하지 않음 */}
+        <Button onPress={addGoalHandler} title='Add Goal'/>
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>List of goals...</Text>
       </View>
     </View>
@@ -17,17 +30,27 @@ export default function App() {
 // 아래에 정의되었지만 jsx에서 사용할 수 있는 건 전체 코드 파일이 파싱된 후에 실행되기 때문
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50
+    flex:1,
+    paddingTop: 50,
+    paddingHorizontal: 16
   },
   inputContainer: {
+    flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
   },
   textInput: {
     borderWidth: 1,
     borderColor: '#cccccc',
-    width: '80%',
+    width: '70%',
     marginRight: 8,
     padding: 8,
+  },
+  goalsContainer: {
+    flex: 4,
   }
 });
