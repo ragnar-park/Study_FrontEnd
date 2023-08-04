bbe17732878268f3d202bbc0a865a3d4a70faf23
ragnar <mystparkkorea@gmail.com>
2023-08-04 13:14:02 +0900

    GoalItem 삭제 적용

--- ReactNative/RNBasic/App.js
+++ ReactNative/RNBasic/App.js
@@ -11,7 +11,13 @@ export default function App() {
     // 새 상태가 이전 상태에 의존하는 방법
     // setCourseGoals([...courseGoals, enteredGoalText]);
     //  상태 업데이트가 비동기적으로 처리되는 상황에서 이전 상태를 보장하는 방법
-    setCourseGoals(courrentCourseGoals => [...courrentCourseGoals, {text: enteredGoalText, id: Math.random().toString()}]);
+    setCourseGoals(currentCourseGoals => [...currentCourseGoals, {text: enteredGoalText, id: Math.random().toString()}]);
+  };
+
+  const deleteGoalHandler = (id) => {
+    setCourseGoals(cuurrentCourseGoals => {
+      return cuurrentCourseGoals.filter((goal) => goal.id !== id);
+    });
   };
 
   return (
@@ -22,7 +28,7 @@ export default function App() {
       <FlatList 
         data={courseGoals} 
         renderItem={(itemData) => {
-          return <GoalItem text={itemData.item.text}/>;
+          return <GoalItem text={itemData.item.text} id={itemData.item.id} onDeleteItem={deleteGoalHandler}/>;
         }}
        keyExtractor={(item, index) => {
         return item.id;
