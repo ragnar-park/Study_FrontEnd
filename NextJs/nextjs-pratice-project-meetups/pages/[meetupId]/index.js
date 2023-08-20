2b3628b898fe6457659bfbc8aa58cd5139ec12bc
ragnar <mystparkkorea@gmail.com>
2023-08-20 17:00:05 +0900

    모임 약속 목록 & 모임 약속 추가 form & 래퍼 컴포넌트

--- NextJs/nextjs-pratice-project-meetups/pages/[meetupId]/index.js
+++ NextJs/nextjs-pratice-project-meetups/pages/[meetupId]/index.js
@@ -1,1 +1,14 @@
+import { Fragment } from "react"
+import MeetupDetail from '../../components/meetups/MeetupDetail'
+
 // our-domain.com/[meetupid]
+const MeetupDetails = () => {
+    return (<MeetupDetail 
+        image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg"
+        title="First Meetup"
+        address="Some Street 5, Soxme City"
+        description="This is a first meetup"
+    />);
+};
+
+export default MeetupDetails;
