9e85b6fdf5d220a260766eb57bf70ce5db68b810
ragnar <mystparkkorea@gmail.com>
2023-10-10 20:08:47 +0900

    전체 레이아웃 설정

---
+++ NextJs/nextMasteryClass/section1/components/Layout.js
@@ -0,0 +1,12 @@
+import style from "./Layout.module.css";
+
+export default function Layout ({ children }) {
+    return (
+        <div>
+            <header className={style.header}>NARAS 🌎</header>
+            <main className={style.main}>
+                {children}
+            </main>
+        </div>
+    )
+};
